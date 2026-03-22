2917f05b06ada837a1c72647cef735dab9fcbafe
Zaid Khan <khanzaid75005@gmail.com>
2026-03-22 11:49:54 +0530

    fix Bugs

---
+++ src/main/java/com/project/headless_cms/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.project.headless_cms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // allow frontend
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
